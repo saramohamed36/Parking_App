86e355cc01c1eeab1baa1b413524cc96dc9cd9d6
Sara Mohamed <saramohamedwesch@gmail.com>
2026-09-08 23:53:26 +0300

    Add files via upload

---
+++ parkingapp/lib/Core/Theme/AppAssets.dart
@@ -0,0 +1,14 @@
+class Appassets {
+  static const imagesPath = 'assets/images/';
+  static const iconsPath = 'assets/svgs/';
+  // Images
+  //static const String splashImage = '${imagesPath}splash.png';
+  //static const String onboardingImage = '${imagesPath}onboarding.png';
+
+  //Icons
+  static const String homeIcon = '${iconsPath}home.svg';
+  static const String mapIcon = '${iconsPath}map.svg';
+  static const String bookingIcon = '${iconsPath}booking.svg';
+  static const String profileIcon = '${iconsPath}profile.svg';
+  static const String notificationIcon = '${iconsPath}notification.svg';
+}
---
+++ parkingapp/lib/Features/Splash/SplashScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
