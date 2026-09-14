5c1f416538867c2cf3ebce877c4f39a5d4c689bf
Sara Mohamed <saramohamedwesch@gmail.com>
2026-09-14 19:20:00 +0300

    Delete parkingapp directory

--- parkingapp/android/app/src/main/kotlin/com/example/parkingapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.parkingapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- parkingapp/lib/Core/Routing/Routes.dart
+++
@@ -1,4 +0,0 @@
-class Routes {
-  static const String splashScreen = '/';
-  static const String onBoardeingScreen = '/onboarding';
-}
